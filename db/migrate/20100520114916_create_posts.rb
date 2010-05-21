458fa26b3e1b9ce2d5efa9f8caa9c83b82ad4143
ben <ben@nerdlabor.de>
2010-05-22 00:03:38 +0200

    only show time fields in form

--- db/migrate/20100520114916_create_posts.rb
+++ db/migrate/20100520114916_create_posts.rb
@@ -2,9 +2,9 @@ class CreatePosts < ActiveRecord::Migration
   def self.up
     create_table :posts do |t|
       t.date :date_from
-      t.time :time_from, :default => :null
+      t.time :time_from, :default => nil
       t.date :date_to
-      t.time :time_to, :default => :null
+      t.time :time_to, :default => nil
       t.string :title
       t.text :short_text
       t.boolean :published
