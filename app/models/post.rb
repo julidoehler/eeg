6d98b66fba37b61f0408bd9534d745b2d209cf04
ben <ben@nerdlabor.de>
2010-05-24 16:53:05 +0200

    added friendly_id gem functionality for nice urls

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  has_friendly_id :title, :use_slug => true, :approximate_ascii => true, :ascii_approximation_options => :german
+
   def has_time_from
   end
   def has_time_to
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100520114916) do
+ActiveRecord::Schema.define(:version => 20100524135102) do
 
   create_table "posts", :force => true do |t|
     t.date     "date_from"
@@ -25,5 +25,17 @@ ActiveRecord::Schema.define(:version => 20100520114916) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "slugs", :force => true do |t|
+    t.string   "name"
+    t.integer  "sluggable_id"
+    t.integer  "sequence",                     :default => 1, :null => false
+    t.string   "sluggable_type", :limit => 40
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "slugs", ["name", "sluggable_type", "sequence", "scope"], :name => "index_slugs_on_n_s_s_and_s", :unique => true
+  add_index "slugs", ["sluggable_id"], :name => "index_slugs_on_sluggable_id"
 
 end
