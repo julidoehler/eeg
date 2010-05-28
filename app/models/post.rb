65d2e6aa7eb208ddda226208cc82a49445d36b51
ben <ben@nerdlabor.de>
2010-05-28 10:01:12 +0200

    updated the views of post and picture a bit, but there's a bug in the edit/update method of post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ActiveRecord::Base
-  has_one :picture
+  belongs_to :picture
   has_friendly_id :title, :use_slug => true, :approximate_ascii => true, :ascii_approximation_options => :german
   
   validates_presence_of :title
-  validates_associated :picture
+  validates_associated :picture, :on => :create 
     
   def picture_attributes=(picture_attributes)
     picture_attributes.each do |attributes|
