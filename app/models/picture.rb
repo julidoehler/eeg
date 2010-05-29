ba8401b355c699b66d13ee0f7121574e8d97f20e
ben <ben@nerdlabor.de>
2010-05-29 14:42:03 +0200

    thumbs width will be resized to 150px

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -6,7 +6,7 @@ class Picture < ActiveRecord::Base
   validates_attachment_content_type :data, :content_type => ['image/jpeg', 'image/pjpeg', 'image/jpg', 'image/png', 'image/gif']
   validates_attachment_size :data, :less_than => 5.megabytes
 
-  has_attached_file :data, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :data, :styles => { :medium => "300x300>", :thumb => "150" }
   
   #validates if there is a gallery with the given id
   def existence_of_gallery_id
