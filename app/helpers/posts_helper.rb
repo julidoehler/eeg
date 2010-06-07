d012dee350b3eecda3bff7452a35f3441fe92ed4
ben <ben@nerdlabor.de>
2010-06-07 18:31:54 +0200

    new we can specify which content_type we add dynamically to the post new/edit views

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,9 +4,9 @@ module PostsHelper
     fields_for("post[#{prefix}_element_attributes][]", element, &block)
   end
 
-  def add_element_link(name)
+  def add_element_link(name,content_type)
     link_to_function name do |page|
-        page.insert_html :bottom, :elements, :partial => 'element', :object => Element.new
+        page.insert_html :bottom, :elements, :partial => 'element', :object => Element.new, :locals => {:content_type => content_type}
     end
   end
 end
