822252ab162758e6084aae28ab48bd8378a220ab
ben <ben@nerdlabor.de>
2010-06-14 11:32:44 +0200

    render the content elements in their own way

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def render_element(e)
+    render :partial => "layouts/" + e.content_type, :locals => {:e => e}
+  end
+  
+  def video_choose(c)
+    if c['youtube.com']
+      render :partial => "layouts/video_youtube", :locals => {:video => c[Regexp.new('v=(.{11})'),1]}
+    elsif c['vimeo.com'] 
+      render :partial => "layouts/video_vimeo", :locals => {:video => c[Regexp.new('[0-9]+')]}
+    else
+      render :inline => "<%= link_to '"+c+"', '"+c+"' %>"
+    end
+  end
 end
