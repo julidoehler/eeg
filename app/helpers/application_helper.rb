61a5d4034faaca35ca3b1ef714a3f3c735cf1275
ben <ben@nerdlabor.de>
2010-06-14 11:56:35 +0200

    render a gallery content element

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,18 @@ module ApplicationHelper
     render :partial => "layouts/" + e.content_type, :locals => {:e => e}
   end
   
-  def video_choose(c)
+  def render_video(c)
     if c['youtube.com']
       render :partial => "layouts/video_youtube", :locals => {:video => c[Regexp.new('v=(.{11})'),1]}
     elsif c['vimeo.com'] 
       render :partial => "layouts/video_vimeo", :locals => {:video => c[Regexp.new('[0-9]+')]}
     else
-      render :inline => "<%= link_to '"+c+"', '"+c+"' %>"
+      render :partial => "layouts/video_link", :locals => {:video => c}
     end
   end
+  
+  def render_gallery(c)
+    gallery = Gallery.find(c)
+    render :partial => "layouts/gallery_link", :locals => {:p => gallery.pictures.first, :l => gallery}
+  end  
 end
