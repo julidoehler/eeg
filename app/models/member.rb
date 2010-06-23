3f4217daa9eed6b3801977c56ee90f11d0fbf3bd
juliane <juliane2503@yahoo.de>
2010-06-24 00:19:37 +0800

    validation

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,6 +2,6 @@ class Member < ActiveRecord::Base
   belongs_to :picture
    
   validates_presence_of :lastname, :firstname, :information
-  #validates_format_of :lastname, :firstname, :with => /^[A-Za-z]+$/i, :message => "only uppercase and lowercase letters"
+  validates_format_of :lastname, :firstname, :with => /^[A-Za-z]+$/i, :message => "only uppercase and lowercase letters"
   validates_associated :picture, :on => :create
 end
