cf40a61b292fe2f652c0c3ea62a54ea219c02dd2
ben <ben@nerdlabor.de>
2010-06-24 14:11:02 +0200

    ...

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def contact
   end
+  
   def contact_send_mail
     @mail ||= params[:mail]
     flash[:notice] = "Please fill in everything!"
@@ -10,19 +11,25 @@ class PagesController < ApplicationController
     end
     redirect_to :action => 'contact'
   end
+  
   def search
     @search = params[:id]
   end
+  
   def schedule
     @dates = Post.find(:all, :select => "date_from, title") + projects = Project .find(:all, :select => "date_from, title")
     @dates.sort! { |a,b| a.date_from <=> b.date_from }
   end
+  
   def profile
   end
+  
   def imprint
   end
+  
   def directions
   end
+  
   def archive
   end
 end
