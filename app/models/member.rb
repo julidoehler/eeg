7be3b4b114dd074ae25dc8b98a56a78cb53eed64
ben <ben@nerdlabor.de>
2010-07-05 14:05:00 +0200

    added elements to member

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,15 @@
+require 'element_methods'
 class Member < ActiveRecord::Base
-  belongs_to :picture
+  include Element_methods
+  acts_as_taggable
+  belongs_to :picture, :dependent => :destroy
+  has_many :elements, :foreign_key => 'parent_id', :dependent => :destroy, :conditions => {:parent_type => 'member'}
+  has_friendly_id :lastname, :use_slug => true, :approximate_ascii => true, :ascii_approximation_options => :german
    
   validates_presence_of :lastname, :firstname, :information, :position
   validates_format_of :lastname, :firstname, :with => /^[A-Za-z]+$/i, :message => "only uppercase and lowercase letters"
   validates_associated :picture, :on => :create
+  validates_associated :elements
+  
+  after_update :save_elements
 end
