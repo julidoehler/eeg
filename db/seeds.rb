374544497f4b811a93bd5fb346f4bae94bffa492
ben <ben@nerdlabor.de>
2010-07-12 14:24:07 +0200

    added user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,4 @@ Gallery.create(:title => 'Projects Gallery')
 Gallery.create(:title => 'Members Gallery')
 
 User.create(:email => 'ben@eexistence.de', :password => 'ben', :email_confirmed => true)
+User.create(:email => 'adrian@eexistence.de', :password => 'adrian', :email_confirmed => true)
